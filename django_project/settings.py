deef07c7275359cd5e136bd14d69ac70b96c87b4
Ivan Danylenko <danylenko.ivan11@gmail.com>
2020-12-01 17:17:39 +0200

    debug true

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^!#z4ul=!rxozleb&^#wvae&y7upwwy#ud_bgl3-$_)y0=@st!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://fullfeaturedwebapp.herokuapp.com']
 
